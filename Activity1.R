53e872ad9a8ed61312fb6a8e523a2e6ffeb62aaa
Zydrick Ceniza <zydrick.ceniza@gmail.com>
2024-01-25 08:46:32 +0000

    update 2 due to server error

--- Activity1.R
+++ Activity1.R
@@ -7,7 +7,7 @@ library(kableExtra)
 
 polite::use_manners(save_as = 'polite_scrape.R')
 
-url <- 'https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172584&ref=nav_em__nav_desktop_sa_intl_scanners_0_2_6_12'
+url <- 'https://www.amazon.com/s?i=specialty-aps&bbn=16225009011&rh=n%3A%2116225009011%2Cn%3A172541&ref=nav_em__nav_desktop_sa_intl_headphones_0_2_5_8'
 session <- bow(url,
                user_agent = "Educational")
 
@@ -15,15 +15,18 @@ session <- bow(url,
 Des_list <- scrape(session) %>%
   html_nodes('span.a-size-base-plus.a-color-base.a-text-normal') %>% 
   html_text
-
+desl<-length(Des_list)
 
 
 price_list <- scrape(session) %>%
-  html_nodes('span.a-price-whole') %>% 
+  html_nodes('span.a-price') %>% 
   html_text
+split_df <- strsplit((price_list),"$",fixed = TRUE)
+split_df <- data.frame(do.call(rbind,split_df))
+split_df<-split_df[-2]
 
-price_df<-price_list
-price_df<-price_df[1:24]
+price_df<-split_df
+price_df<-price_df[1:desl,2]
 
 
 
@@ -31,7 +34,7 @@ rate_list <- scrape(session) %>%
   html_nodes('span.a-icon-alt') %>% 
   html_text
 Rate<-rate_list
-Rate<-Rate[1:24]
+Rate<-Rate[1:desl]
 Rate<-as.data.frame(Rate)
 
 
@@ -39,86 +42,88 @@ Rate<-as.data.frame(Rate)
 numberofreviews_list <- scrape(session) %>%
   html_nodes('span.a-size-base.s-underline-text') %>% 
   html_text
-numberofreviews_list<-numberofreviews_list[1:24]
+numberofreviews_list<-numberofreviews_list[1:desl]
 Category1<-cbind(Des_list,price_df,Rate,numberofreviews_list)
 colnames(Category1)<-c("Description","Price","Ratings","Number of Reviews")
 Category1
 #-----------------------------------------------------------------------------
 
-url <- 'https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172635&ref=nav_em__nav_desktop_sa_intl_printers_0_2_6_11'
-
+url <- 'https://www.amazon.com/s?i=specialty-aps&bbn=4954955011&rh=n%3A4954955011%2Cn%3A%212617942011%2Cn%3A2237329011&ref=nav_em__nav_desktop_sa_intl_needlework_0_2_8_8'
 session <- bow(url,
                user_agent = "Educational")
-
-
-title <- character(0)
 
 
 Des_list <- scrape(session) %>%
   html_nodes('span.a-size-base-plus.a-color-base.a-text-normal') %>% 
   html_text
-
+desl<-length(Des_list)
 
 
 price_list <- scrape(session) %>%
-  html_nodes('span.a-price-whole') %>% 
+  html_nodes('span.a-price') %>% 
   html_text
-price_df<-price_list
-price_df<-price_df[1:24]
+split_df <- strsplit((price_list),"$",fixed = TRUE)
+split_df <- data.frame(do.call(rbind,split_df))
+split_df<-split_df[-2]
+
+price_df<-split_df
+price_df<-price_df[1:desl,2]
 
 
 
 rate_list <- scrape(session) %>%
   html_nodes('span.a-icon-alt') %>% 
   html_text
-
 Rate<-rate_list
-Rate<-Rate[1:24]
+Rate<-Rate[1:desl]
 Rate<-as.data.frame(Rate)
+
 
 
 numberofreviews_list <- scrape(session) %>%
   html_nodes('span.a-size-base.s-underline-text') %>% 
   html_text
-numberofreviews_list<-numberofreviews_list[1:24]
+numberofreviews_list<-numberofreviews_list[1:desl]
 Category2<-cbind(Des_list,price_df,Rate,numberofreviews_list)
 Category2
 #--------------------------------------------------------------------
 
-url <- 'https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A2348628011&ref=nav_em__nav_desktop_sa_intl_tablet_accessories_0_2_6_14'
+url <- 'https://www.amazon.com/s?i=specialty-aps&bbn=16225009011&rh=n%3A%2116225009011%2Cn%3A502394&ref=nav_em__nav_desktop_sa_intl_camera_and_photo_0_2_5_3'
 session <- bow(url,
                user_agent = "Educational")
-
-
-title <- character(0)
 
 
 Des_list <- scrape(session) %>%
   html_nodes('span.a-size-base-plus.a-color-base.a-text-normal') %>% 
   html_text
-
+desl<-length(Des_list)
 
 
 price_list <- scrape(session) %>%
-  html_nodes('span.a-price-whole') %>% 
+  html_nodes('span.a-price') %>% 
   html_text
-price_df<-price_list
-price_df<-price_df[1:24]
+split_df <- strsplit((price_list),"$",fixed = TRUE)
+split_df <- data.frame(do.call(rbind,split_df))
+split_df<-split_df[-2]
+
+price_df<-split_df
+price_df<-price_df[1:desl,2]
 
 
 
 rate_list <- scrape(session) %>%
   html_nodes('span.a-icon-alt') %>% 
   html_text
-
 Rate<-rate_list
-Rate<-Rate[1:24]
+Rate<-Rate[1:desl]
 Rate<-as.data.frame(Rate)
+
 
 
 numberofreviews_list <- scrape(session) %>%
   html_nodes('span.a-size-base.s-underline-text') %>% 
   html_text
-numberofreviews_list<-numberofreviews_list[1:24]
+numberofreviews_list<-numberofreviews_list[1:desl]
 Category3<-cbind(Des_list,price_df,Rate,numberofreviews_list)
 Category3
+
